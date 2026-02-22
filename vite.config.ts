7c66d5aa9aecd68dac893128d15973d283cea782
qixiao-debug <qi.xiao@ucdconnect.ie>
2026-02-22 22:34:49 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/Social-media/', // 
+    base: '/Social-media/',
 
     plugins: [react(), tailwindcss()],
 
