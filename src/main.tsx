dd899a1842d5bb2d67ecb63cd67c6f6261295d3f
qixiao-debug <qi.xiao@ucdconnect.ie>
2026-02-24 15:38:08 +0000

    Integrate Google Analytics tracking script
    Add Google Analytics script for tracking.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,13 @@
+const script = document.createElement("script");
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-CPLZ8WX1F2";
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-CPLZ8WX1F2');
+
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
