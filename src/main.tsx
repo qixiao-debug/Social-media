c7af7ebf4d80ff2066bf4d413b80c79f86b988fc
qixiao-debug <qi.xiao@ucdconnect.ie>
2026-02-24 15:46:55 +0000

    Refactor main.tsx to use React and gtag setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
-const script = document.createElement("script");
-script.async = true;
-script.src = "https://www.googletagmanager.com/gtag/js?id=G-CPLZ8WX1F2";
-document.head.appendChild(script);
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-CPLZ8WX1F2');
-
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
+
+const script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-CPLZ8WX1F2';
+document.head.appendChild(script);
+
+
+window.dataLayer = window.dataLayer || [];
+window.gtag = function () {
+  window.dataLayer.push(arguments);
+};
+
+window.gtag('js', new Date());
+window.gtag('config', 'G-CPLZ8WX1F2');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
